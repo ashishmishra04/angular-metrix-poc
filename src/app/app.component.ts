e3d3273327d11c107ee342386ef41134bfac5467
Ashish Mishra <amishra@kruxanalytics.com>
2023-03-13 15:37:13 -0600

    Added Auth login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {AuthService, IdToken, User} from "@auth0/auth0-angular";
+import {DOCUMENT} from "@angular/common";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  user$ = new BehaviorSubject<User| null>(null);
+  idToken : IdToken | null | undefined = null;
+  accessToken : any = null;
+  constructor(public authService: AuthService,
+              @Inject(DOCUMENT) private doc: Document) {
+    this.authService.user$.subscribe((user) => {
+      console.log('USER-UPDATED', user);
+      this.idToken = null;
+      this.accessToken = null;
+      if (user) {
+        this.user$.next(user);
+        this.authService.idTokenClaims$.subscribe((token) => {
+          console.log('TOKEN', token);
+          this.idToken = token;
+        });
+
+        this.authService.getAccessTokenSilently({detailedResponse: true} )
+          .subscribe((accessToken) => {
+            console.log('ACCESS-TOKEN', accessToken);
+            this.accessToken = accessToken;
+          });
+
+      } else {
+        this.user$.next(null);
+      }
+    }
+  );
+  }
+  onLogin() {
+    this.authService.loginWithRedirect();
+  }
+
+  onLogout() {
+    this.authService.logout({ logoutParams: { returnTo: this.doc.location.origin } });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthModule} from "@auth0/auth0-angular";
+import {NgxJsonViewerModule} from "ngx-json-viewer";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxJsonViewerModule,
+    AuthModule.forRoot({
+      domain: 'dev-krux.us.auth0.com',
+      clientId: 'TCikfPsnU1dFxGonhqLChizj3Gdy5kMS',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        userAuthProfile:'http://s-kruxmetrix.kruxanalytics.com/auth0/GetUserAuthProfile'
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
